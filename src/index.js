202dfab7d78e572dd6f365f0460558c7ec3fc644
Andrew <andrewmoren93@gmail.com>
2023-05-30 00:00:50 +0300

    create structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./components/styles/index.css";
+import "./styles/index.css";
 import App from "./components/App/App";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
