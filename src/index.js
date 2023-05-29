e23202c829a8335125febfa5668265ec94095bbb
Andrew <andrewmoren93@gmail.com>
2023-05-29 23:49:00 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./components/styles/index.css";
 import App from "./components/App/App";
 
 createRoot(document.getElementById("root")).render(<App />);
