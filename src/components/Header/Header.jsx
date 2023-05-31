9090164de1a171993b30e3d368b21cc63017dca2
Andrew <andrewmoren93@gmail.com>
2023-05-31 23:09:26 +0300

    create hearer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../utils/routes";
+
+import styles from "../../styles/Header.module.css";
+import LOGO from "../../images/logo.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt="logo" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
