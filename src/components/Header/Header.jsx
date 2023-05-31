3e6beb50047d0f26723faff99f2fdc762de98250
Andrew <andrewmoren93@gmail.com>
2023-05-31 23:22:36 +0300

    create hearer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { ROUTES } from "../../utils/routes";
 
 import styles from "../../styles/Header.module.css";
 import LOGO from "../../images/logo.svg";
+import AVATAR from "../../images/avatar.jpg";
 
 const Header = () => {
   return (
@@ -12,6 +13,24 @@ const Header = () => {
         <Link to={ROUTES.HOME}>
           <img src={LOGO} alt="logo" />
         </Link>
+      </div>
+
+      <div className={styles.info}>
+        <div className={styles.user}>
+          <div
+            className={styles.avatar}
+            style={{ backgroundImage: `url(${AVATAR})` }}
+          />
+          <div className={styles.username}>Guest</div>
+        </div>
+
+        <form className={styles.form}>
+          <div className={styles.icon}>
+            <svg className="icon">
+              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
+            </svg>
+          </div>
+        </form>
       </div>
     </div>
   );
