5418f2d3d60a3f3376b347498b411898864addb9
Andrew <andrewmoren93@gmail.com>
2023-06-02 22:55:56 +0300

    end footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/Footer.module.css";
+import { ROUTES } from "../../utils/routes";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import LOGO from "../../images/logo.svg";
+
+const Footer = () => (
+  <section className={styles.footer}>
+    <div className={styles.logo}>
+      <Link to={ROUTES.HOME}>
+        <img src={LOGO} alt="logo" />
+      </Link>
+    </div>
+
+    <div className={styles.rights}>Developed by Andrew Moren</div>
+
+    <div className={styles.socials}>
+      <a
+        href="https://instagram.com/andrewmoren"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+        </svg>
+      </a>
+
+      <a
+        href="https://instagram.com/andrewmoren"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+        </svg>
+      </a>
+
+      <a
+        href="https://instagram.com/andrewmoren"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+        </svg>
+      </a>
+    </div>
+  </section>
+);
 
 export default Footer;
