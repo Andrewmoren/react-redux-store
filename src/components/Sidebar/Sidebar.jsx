8a17f7cfe2cb1f300f81e7a4760feafd4799ba15
Andrew <andrewmoren93@gmail.com>
2023-06-06 14:20:57 +0300

    sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import styles from "../../styles/Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div className={styles.sidebar}>
+    <section className={styles.sidebar}>
       <div className={styles.title}>CATEGORIES</div>
-    </div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>Link</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a href="/help" target="_blank" className={styles.link}>
+          Help
+        </a>
+        <a
+          href="/terms"
+          target="_blank"
+          className={styles.link}
+          style={{ textDecoration: "underline" }}
+        >
+          Terms & Condition
+        </a>
+      </div>
+    </section>
   );
 };
 
