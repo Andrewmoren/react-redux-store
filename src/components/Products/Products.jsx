eec5be1670b25dfaa58604348c5f4868f8069805
Andrew <andrewmoren93@gmail.com>
2023-06-11 23:06:21 +0300

    product 1

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from "../../styles/Products.module.css";
 
-const Products = ({ title = "" }) => {
+const Products = ({ title, products = [] }) => {
   return (
     <section>
-      <h2>{title}</h2>
+      <h2>{title && <h2>{title}</h2>}</h2>
+      {products.map(({ id, images, title, category: { name: cat }, price }) => (
+        <Link to={`/products/${id}`} key={id}></Link>
+      ))}
     </section>
   );
 };
