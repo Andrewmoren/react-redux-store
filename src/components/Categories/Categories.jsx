3f165fec564475fab5a0e6489d44868f66b85071
Andrew <andrewmoren93@gmail.com>
2023-06-12 07:00:52 +0300

    add categories

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "../../styles/Categories.module.css";
+
+const Categories = () => {
+  return <div>Categories</div>;
+};
+
+export default Categories;
