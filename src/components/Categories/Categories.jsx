5384cd8eeb8d2ec609a1b332e93044917e7d3898
Andrew <andrewmoren93@gmail.com>
2023-06-13 23:14:32 +0300

    add && create products

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 import styles from "../../styles/Categories.module.css";
 
-const Categories = () => {
-  return <div>Categories</div>;
+const Categories = ({ title, products = [], amount }) => {
+  const list = products.filter((_, i) => i < amount);
+  return (
+    <section className={styles.section}>
+      <h2>{title}</h2>
+    </section>
+  );
 };
 
 export default Categories;
