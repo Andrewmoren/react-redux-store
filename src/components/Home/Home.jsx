231e539330a35b329615c0c769abc76c2937168e
Andrew <andrewmoren93@gmail.com>
2023-06-21 23:54:54 +0300

    add && create products 1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,13 +5,13 @@ import Poster from "../Poster/Poster";
 import Products from "../Products/Products";
 
 const Home = () => {
-  const { products, categories } = useSelector(({ state }) => state);
+  const { products, categories } = useSelector((state) => state);
   return (
-    <section>
-      <Poster />
-      <Products products={products.list} amount={5} title="Trending" />
-      <Categories products={categories.list} amount={5} title="Worth seeing" />
-    </section>
+    // <section>
+    //   <Poster />
+    //   <Products products={products.list} amount={5} title="Trending" />
+    //   <Categories products={categories.list} amount={5} title="Worth seeing" />
+    // </section>
   );
 };
 
