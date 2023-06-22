126b00fb8fa09f728c0c8f4282184f5549aef9a2
Andrew <andrewmoren93@gmail.com>
2023-06-22 23:17:53 +0300

    add banner1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,11 +7,11 @@ import Products from "../Products/Products";
 const Home = () => {
   const { products, categories } = useSelector((state) => state);
   return (
-    // <section>
-    //   <Poster />
-    //   <Products products={products.list} amount={5} title="Trending" />
-    //   <Categories products={categories.list} amount={5} title="Worth seeing" />
-    // </section>
+    <section>
+      <Poster />
+      <Products products={products.list} amount={5} title="Trending" />
+      <Categories products={categories.list} amount={5} title="Worth seeing" />
+    </section>
   );
 };
 
