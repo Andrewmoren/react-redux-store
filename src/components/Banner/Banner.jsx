02cfbc9ee15c611103e0c85e30c04f80db1b1db2
Andrew <andrewmoren93@gmail.com>
2023-06-23 06:39:30 +0300

    add banner1

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import styles from "../../styles/Home.module.css";
+import bannerImg from "../../images/banner.png";
+
 const Banner = () => {
   return <div>Banner</div>;
 };
