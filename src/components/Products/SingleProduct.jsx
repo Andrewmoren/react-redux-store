1ea4046c5cb22cf22322502c4a443a9ade7b43ee
Andrew <andrewmoren93@gmail.com>
2023-07-14 06:52:23 +0300

    create single product

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -22,7 +22,6 @@ const SingleProduct = () => {
     if (!isFetching && !isLoading && !isSuccess) {
       navigate(ROUTES.HOME);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoading, isFetching, isSuccess]);
 
   useEffect(() => {
